8ac1a88ff5160a517fc6e649f2cb1040d04d435e
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-04-19 15:05:37 +0000

    first commit

---
+++ src/components/Tour.js
@@ -0,0 +1,33 @@
+// Tour.js
+import React, { useState } from 'react';
+
+const Tour = ({ id, name, info, image, price, removeTour }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  const toggleInfo = () => {
+    setShowInfo(!showInfo);
+  };
+
+  return (
+    <article className="tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>
+          {showInfo ? info : `${info.substring(0, 200)}...`}
+          <button onClick={toggleInfo}>
+            {showInfo ? 'See Less' : 'Read More'}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          Not Interested
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default Tour;
---
+++ src/components/Tours.js
@@ -0,0 +1,15 @@
+// Tours.js
+import React from 'react';
+import Tour from './Tour';
+
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      {tours.map((tour) => (
+        <Tour key={tour.id} {...tour} removeTour={removeTour} />
+      ))}
+    </section>
+  );
+};
+
+export default Tours;
